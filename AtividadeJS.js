305080819ee690b81cf3415096e6e4a6b920f059
Laura Rodrigues de Paula <laura59978506@edu.df.senac.br>
2026-03-02 21:33:45 -0300

    Atvidade de funções no JS

---
+++ AtividadeJS.js
@@ -0,0 +1,118 @@
+
+
+/*/ 
+function media3(n1, n2, n3)  {
+const resultado = (n1 + n2 + n3) / 3
+return console.log(resultado)
+
+
+/* 
+media3 (34,36,38)
+function areaTriangulo (base, altura) {
+ const resultado = (base * altura) / 2
+ return console.log (resultado) }
+areaTriangulo (42,24)
+
+// 
+
+//
+
+function precoComAcrescismo(preco, desconto) {
+  const resultado = preco - (preco * desconto) / 100
+  return console.log(resultado)
+}
+console.log(precoComAcrescismo(89, 12))
+
+/*
+
+function restoDivisao(a, b) {
+  const resultado = (a % b)
+  return console.log(resultado)
+}
+console.log(restoDivisao(1, 6))
+
+/-----------------------------------------------------------------
+
+function notaFinal(APROVADO, REFORÇO) {
+
+  return APROVADO >= +69 && REFORÇO >= -68
+}
+const aprovado = true
+const reforço = false
+
+if (notaFinal( aprovado, reforço)) {
+  console.log("de ferias")
+} else {
+  console.log("liga pra mamis")
+}
+/*
+function alunosPorcentagem(aprovado, reforco, porcentagem){
+  const resultado = (aprovado * reforco) / porcentagem
+  return resultado
+}
+
+const aprovado = 100
+const reforco = 50
+const porcentagem = 40
+
+if (alunosPorcentagem(aprovado, reforco, porcentagem ) >= 70){
+  console.log ('Se livrou')
+}else {
+  console.log ('Estudar mais')
+}
+
+function percentual(aprovados,total) {
+  return (aprovados / total) * 100
+
+}
+const aprovados = 15
+const total = 25
+if (total <= 0 || aprovados > total) {
+console.log ("Dados inválidos")
+} else {
+  const resposta = percentual (aprovados, total)
+if (resposta >= 70) {
+  console.log ("Aprovados: $ {resposta}% ")
+    { else {
+      console.log ("reprovados: $ {resposta}&")
+    }
+
+
+function SaldoDeopisdaCompra(saldoAtual, valorCompra) {
+ return saldoAtual - valorCompra
+}
+const saldoAtual = 150
+const valorCompra = 50
+
+if (valorCompra <=0) {
+console.log ('Valor inválido')
+} else{
+  const saldoFinal = saldoDepoisDaCompra (saldoAtual, valorCompra)
+  if(saldoFinal > 0 && valorCompra <= saldoFinal) {
+    console.log ("Pode fazer compras")
+  } else {
+    console.log ("Falta din din")
+  }
+ }
+
+*/ 
+
+function aplicarCupom (preco, cupom) {
+  return preco - (preco* (cupom / 100))
+}
+
+const preco = 150
+const cupom = 10
+
+const aplicarDesconto = aplicarCupom (preco, cupom)
+
+if (precofinal > 0) {
+  if (cupom === 0) {
+console.log ("O valor da conta com desconto é ${precoFinal}")
+  }
+} else{
+  console.log ("Valor inválido")
+}
+
+//verificar se não houve desconto e retornar o valor sem desconto
+ 
